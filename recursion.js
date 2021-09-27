4fb7a25d376a888d6a9448bf7e24485197b8638e
Volodymyr <84179423+Volodymyr-AI@users.noreply.github.com>
2021-09-27 22:04:25 +0300

    Update recursion.js

--- recursion.js
+++ recursion.js
@@ -51,3 +51,16 @@ console.log(sumSalaries(company));
 /* return 6300 */
 /*------------------------------*/
 
+/*---------factorial------------*/
+function factorialize(num, factorial = 1) {
+  if (num == 0) {
+    return factorial;
+  } else {
+    return factorialize(num - 1, factorial * num);
+  }
+}
+
+factorialize(5);
+/* return 120 = 5*4*3*2*1 */
+/*------------------------------*/
+
